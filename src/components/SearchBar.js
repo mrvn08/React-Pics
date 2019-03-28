428f77c6175a037559308ea4d86fbaa6af1ca31e
Marvin <marvinlimson2007@gmail.com>
2019-03-28 13:59:29 +0000

    Added in more styling and on change function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    state = {searchText: null}
+
+    onSearchInputChange(event){
+        console.log(event.target.value);
+        this.setState({searchText: event.target.value});
+    }
+
     render(){
         return (
-            <div>
-                <form>
-                    <div class="ui input">
-                        <input type="text" placeholder="Search..."/>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <div className="ui input">
+                            <input type="text" onChange={this.onSearchInputChange} placeholder="Search..."/>
+                        </div>
                     </div>
                 </form>    
             </div>
