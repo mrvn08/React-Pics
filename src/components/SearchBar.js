0712282c595c94d10c7e4ecf0c37a41a2fff451a
Marvin <marvinlimson2007@gmail.com>
2019-03-29 12:30:03 +0000

    Added onSubmit handler for searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,21 @@ import React from 'react';
 class SearchBar extends React.Component{
     state = {searchText: ''}
 
+    onFormSubmit(event){
+        event.preventDefault();
+    }
+    
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
                         <div className="ui input">
-                            <input type="text" value={this.state.searchText} onChange={(e) => this.setState({searchText: e.target.value})} placeholder="Search..."/>
+                            <input 
+                                type="text" 
+                                value={this.state.searchText} 
+                                onChange={(e) => this.setState({searchText: e.target.value})} placeholder="Search..."/>
                         </div>
                     </div>
                 </form>    
