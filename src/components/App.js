a8231d17bddff830f2e973ff10c2309cbc62bed3
Marvin <marvinlimson2007@gmail.com>
2019-04-01 11:01:41 +0100

    installed axios and added it to app js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from axios;
 import SearchBar from './SearchBar';
 
 export default class App extends React.Component {
     onFormSubmit = (term) => {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params:{ query: term },
+            headers:{
+                Authorization: 'Client-ID d31b43a1633960d67bb432532b56689d873bd74e2f55d95c2d5925c8dbd4bd7f'
+            }
+        });
     }
 
     render(){
