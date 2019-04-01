8d0858bc78b761d6b7094dfbb71f1e800dc70f54
Marvin <marvinlimson2007@gmail.com>
2019-04-01 11:17:57 +0100

    DId an alternate version for an async request

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,14 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 export default class App extends React.Component {
-    onFormSubmit = (term) => {
-        axios.get('https://api.unsplash.com/search/photos', {
+    onFormSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params:{ query: term },
             headers:{
                 Authorization: 'Client-ID d31b43a1633960d67bb432532b56689d873bd74e2f55d95c2d5925c8dbd4bd7f'
             }
-        }).then((response) => {
-            console.log(response.data.results);
-        });
+        })
+        console.log(response.data.results);
     }
 
     render(){
