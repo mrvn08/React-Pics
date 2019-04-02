90b5e29b6eca2f6b7d65952884d9733fda95ef2c
Marvin <marvinlimson2007@gmail.com>
2019-04-02 15:16:55 +0100

    Added image ref in Image Card

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 export default class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount(){
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render(){
-        const {alt_descripton, urls} = this.props.image;
+        const {alt_description, urls} = this.props.image;
         return (
             <div>
                 <img 
                     alt={alt_description}
                     src={urls.regular}
+                    ref={this.imageRef}
                 />
             </div>
         )
